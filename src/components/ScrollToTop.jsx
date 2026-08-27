3d25ddd4498932f9374aeea6c63830970e87cbed
yonimillipro <yonimillipro@gmail.com>
2026-08-27 21:33:50 +0300

    Stabilize-refresh-scroll-position

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,11 +1,15 @@
-import { useEffect } from 'react';
+import { useLayoutEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
+  useLayoutEffect(() => {
+    const resetScroll = () => window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+    resetScroll();
+
+    const animationFrame = window.requestAnimationFrame(resetScroll);
+    return () => window.cancelAnimationFrame(animationFrame);
   }, [pathname]);
 
   return null;
